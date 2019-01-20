71c6ec4e0aed44c8a5bc8f2b6e7feff61601293a
bomj321 <mauricio1314@hotmail.com>
2019-01-20 05:06:02 -0400

    Borrando Datos Importantes

--- public/paypal/config.php
+++ public/paypal/config.php
@@ -6,8 +6,8 @@ require 'autoload.php';
 
 $apiContext = new \PayPal\Rest\ApiContext(
     new \PayPal\Auth\OAuthTokenCredential(
-        'AdvVtorYR0RUOKO6roNDMOuDk9GRz9p96RWgdNoUefMJUPd3XgnLyyt6KVvwbD8wM2uXhfkDcqOOX6kw',     // ClientID
-        'EP5hscVEHsXLE7hdeuUiW36GEUIihrtTAyDRNqPyc9ed72OsqEO0NnHbLrp4RGiVCHWMHQn0rxqsm0IQ'      // ClientSecret
+        '',     // ClientID
+        ''      // ClientSecret
     )
 );
  ?>
